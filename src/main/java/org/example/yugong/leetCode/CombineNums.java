3ebf59e761857d86f013ee518c0f0579ae4449e0
majorJoe <qiaobao@souche.com>
2020-09-15 22:13:03 +0800

    test merge

---
+++ src/main/java/org/example/yugong/leetCode/CombineNums.java
@@ -0,0 +1,15 @@
+package org.example.yugong.leetCode;
+
+import java.util.List;
+
+/**
+ * @author qiaobao
+ * @since 2020-09-08
+ */
+public class CombineNums {
+
+    public List<List<Integer>> combine(int n, int k) {
+
+
+    }
+}
