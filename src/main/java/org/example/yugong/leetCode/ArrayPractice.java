06fad144491e7ee7d6472c3683b9ee5aa66ff24b
majorJoe <qiaobao@souche.com>
2021-01-12 14:06:08 +0800

    threadLocal

--- src/main/java/org/example/yugong/leetCode/ArrayPractice.java
+++ src/main/java/org/example/yugong/leetCode/ArrayPractice.java
@@ -24,9 +24,42 @@ public class ArrayPractice {
     }
 
     public static void main(String[] args) {
-        int i = removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
-        System.out.println(i);
+//        int i = removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4});
+        int maxProfit = maxProfit(new int[]{7,1,5,3,6,4});
+
+        System.out.println(maxProfit);
 
     }
+
+    public static int maxProfit(int[] prices) {
+        // 操作点位
+        int buyIndex = 0;
+        // 当前是否持仓标识
+        boolean holdFlag = false;
+
+        int profit = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (holdFlag) {
+                if (prices[i] > prices[i + 1]) {
+                    holdFlag = false;
+                    profit = profit + (prices[i] - prices[buyIndex]);
+                }else {
+                    if (i == prices.length - 2) {
+                        profit = profit + (prices[i+1] - prices[buyIndex]);
+                        break;
+                    }
+                }
+            } else {
+                if (prices[i] < prices[i + 1]) {
+                    buyIndex = i;
+                    holdFlag = true;
+                }
+            }
+        }
+        return profit;
+    }
+
+
 
 }
