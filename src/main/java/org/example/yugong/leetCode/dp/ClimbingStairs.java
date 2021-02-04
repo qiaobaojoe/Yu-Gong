fb2777c5f02d13d265154522ba5991cc14824a82
majorJoe <qiaobao@souche.com>
2021-02-04 16:56:50 +0800

    feat： 动态规划的思想，这种是递归的方式实现。 存在一个明显的问题，就是重复计算。
    这里就存在一个问题，
    就是所有的递归都可以转换成迭代吗？
    
    迭代的思想就是
    把上一次循环的出参作为下一次循环的入参。
    迭代一定是可以确定层次的！！！
    复杂的递归可以转换成栈的数据操作

--- src/main/java/org/example/yugong/leetCode/dp/ClimbingStairs.java
+++ src/main/java/org/example/yugong/leetCode/dp/ClimbingStairs.java
@@ -7,14 +7,16 @@ package org.example.yugong.leetCode.dp;
 public class ClimbingStairs {
 
     public int climbingStairs(int n) {
-        if (n == 1) {
-            return 1;
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[2] = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
-        if (n == 2) {
-            return 2;
-        }
-        n = climbingStairs(n - 1) + climbingStairs(n - 2);
-        return n;
+        return dp[2];
 
     }
 
