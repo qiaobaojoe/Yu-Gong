46dca826eff36574fe38d869abbdce9ac362681f
majorJoe <qiaobao@souche.com>
2021-02-04 20:21:21 +0800

    feat： 对这个问题进行了升级，我的方法就不能使用了，记录一下思路，普通的减枝也不能解决这个问题。
    状态转化方程？？？

--- src/main/java/org/example/yugong/leetCode/dp/ClimbingStairs.java
+++ src/main/java/org/example/yugong/leetCode/dp/ClimbingStairs.java
@@ -17,12 +17,54 @@ public class ClimbingStairs {
             dp[1] = dp[2];
         }
         return dp[2];
-
     }
+
+    /**
+     * 不能连续跳过两个台阶
+     * 思考了一下，有两种方法：
+     * 1、在计算完成后做减枝，就是算出连续跳两次的方案有多少种
+     * 2、就是穷举出一个固定的序列
+     */
+    public int climbingStairsLimit(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        boolean flag = false;
+
+        for (int i = 3; i <= n; i++) {
+
+            if (flag) {
+                dp[i] = dp[i - 1];
+                flag = false;
+            }else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+                flag = true;
+            }
+        }
+        return dp[n];
+    }
+
+    public int climbingStairsLimit2(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        boolean flag = false;
+
+        for (int i = 3; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + dp[i - 3];
+        }
+        return dp[n];
+    }
+
 
     public static void main(String[] args) {
         ClimbingStairs climbingStairs = new ClimbingStairs();
-        System.out.println(climbingStairs.climbingStairs(5));
+        System.out.println(climbingStairs.climbingStairs(10));
+
+        System.out.println(climbingStairs.climbingStairsLimit(10));
+        System.out.println(climbingStairs.climbingStairsLimit2(10));
 
     }
 
