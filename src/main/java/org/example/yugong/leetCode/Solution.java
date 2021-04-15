59c61927b5530bc2c7dfa15c2a812cea21c94893
majorJoe <qiaobao@souche.com>
2021-04-15 16:16:48 +0800

    feat:zookeeper

--- src/main/java/org/example/yugong/leetCode/Solution.java
+++ src/main/java/org/example/yugong/leetCode/Solution.java
@@ -1,5 +1,7 @@
 package org.example.yugong.leetCode;
 
+import java.util.Arrays;
+
 /**
  * @author qiaobao
  * @since 2021-02-21
@@ -83,6 +85,27 @@ class Solution {
 
         return total;
     }
+
+
+    public String largestNumber(int[] nums) {
+        int n = nums.length;
+        String[] ss = new String[n];
+        for (int i = 0; i < n; i++) {
+            ss[i] = "" + nums[i];
+        }
+        Arrays.sort(ss, (a, b) -> {
+            String sa = a + b, sb = b + a ;
+            return sb.compareTo(sa);
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : ss) sb.append(s);
+        int len = sb.length();
+        int k = 0;
+        while (k < len - 1 && sb.charAt(k) == '0') k++;
+        return sb.substring(k);
+    }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
