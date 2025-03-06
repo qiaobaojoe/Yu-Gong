9a8bb3dce94494b0dbabf05e5a6f94a82721e1a2
majorjoe <qiaobaocn@gmail.com>
2025-03-06 11:00:30 +0800

    feature:初始化

---
+++ src/main/java/org/example/yugong/collection/RangeListLearn.java
@@ -0,0 +1,5 @@
+package org.example.yugong.collection;/**
+ * @author qiaobao
+ * @since 2023/3/1
+*/public class RangeListLearn {
+}
---
+++ src/main/java/org/example/yugong/leetCode/AllNumsZero.java
@@ -0,0 +1,5 @@
+package org.example.yugong.leetCode;/**
+ * @author qiaobao
+ * @since 2024/4/9
+*/public class AllNumsZero {
+}
