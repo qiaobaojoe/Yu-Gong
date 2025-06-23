2c302a7b97f315fa4c34af1543db3dfe28a7cb9d
majorjoe <qiaobaocn@gmail.com>
2025-06-23 12:55:30 +0800

    feature:非常巧妙的解法，还是要考虑相关性的问题

--- src/main/java/org/example/leetcode/june/TwentyTwo.java
+++ src/main/java/org/example/leetcode/june/TwentyTwo.java
@@ -2,6 +2,7 @@ package org.example.leetcode.june;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * @author qiaobao
@@ -15,26 +16,40 @@ public class TwentyTwo {
         int n = pairs.length;
         int[] dp = new int[n];
         dp[0] = 1;
-        for (int i = 1; i <n; i++) {
+        for (int i = 1; i < n; i++) {
             int longestI = 1;
             int[] pairI = pairs[i];
             for (int j = 0; j < i; j++) {
                 int[] pairJ = pairs[j];
-                if (pairI[0] > pairJ[1]){
+                if (pairI[0] > pairJ[1]) {
                     longestI = Integer.max(longestI, dp[j] + 1);
                 }
-
             }
             dp[i] = longestI;
-        }
-//        打印dp数组
-        for (int i : dp) {
-            System.out.println(i);
         }
         return Arrays.stream(dp).max().orElse(0);
 
 
     }
+
+    public int longestSubsequence(int[] arr, int difference) {
+        int n = arr.length;
+        if (n == 1) {
+            return 1;
+        }
+        // 单个元素也可以组成一个子序列
+        // 下一个值就必须要 == i+k
+        // 这里首先是暴力的解法，数组中的每一个元素都可以作为等差子序列的起点或者终点，遍历每一个选择的结果 0(n^2/2)，当然这里或许可以采用记忆集的方式优化
+        // 想一想有没有推导的方式，我觉得是类似最长子序列的
+        HashMap<Integer, Integer> dp = new HashMap<Integer, Integer>(n);
+        int ans = 1;
+        for (int a : arr) {
+            int longestA = dp.getOrDefault(a - difference, 0) + 1;
+            ans = Math.max(ans, longestA);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         TwentyTwo twentyTwo = new TwentyTwo();
         twentyTwo.findLongestChain(new int[][]{
