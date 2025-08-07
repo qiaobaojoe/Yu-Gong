3b47ef87560eef1e91108ad9a01ffc40f6112e93
majorjoe <qiaobaocn@gmail.com>
2025-08-07 17:13:04 +0800

    feature:都是缓存太慢惹的出来事。或者 CPU 的计算资源太宝贵了，在计算机的世界里，CPU 就代表着时间，显然设计者不愿意浪费任何一段时间，它忍受不了内存的读写的龟速，于是有了缓存，有一多核 CPU 的存在，多个 CPU 缓存需要保持一性性，但是同步信息变更也太慢了，于是设计了 storeBuffer，将数据一致性的同步变成了异步，于是有了Java 内存模型的工作内存的存在volitile 保证变量在工作内存的可见性。同时为了提交 CPU 执行中缓存的命中率，编译后的代码的执行顺序可能会变化，为了有限匹配缓存行已经加载的内容。volitile 关键字，提醒编译器变量是异变的，不能优化变量，同时禁止指令重排序

--- src/main/java/org/example/leetcode/august/Five.java
+++ src/main/java/org/example/leetcode/august/Five.java
@@ -26,10 +26,34 @@ public class Five {
 
     }
 
-    public static void main(String[] args) {
-        Five five = new Five();
-        int[] fruits = {3,6,1};
-        int[] baskets = {6,4,7};
-        System.out.println(five.numOfUnplacedFruits(fruits, baskets));
+    static int x = 0, y = 0;
+    static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; true; i++) {
+            x = 0; y = 0; a = 0; b = 0;
+            Thread one = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    a = 1;
+                    x = b;
+                }
+            });
+            Thread other = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b = 1;
+                    y = a;
+                }
+            });
+            one.start();
+            other.start();
+            one.join();
+            other.join();
+            if (x == 0 && y == 0) {
+                System.out.println("i=" + i);
+                break;
+            }
+        }
     }
 }
