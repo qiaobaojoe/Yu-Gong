4e18942d671b802ad4a56cda48639e3ca8e34f82
majorjoe <qiaobaocn@gmail.com>
2025-09-10 00:32:40 +0800

    feature：我用迭代的方式做出来

--- src/main/java/org/example/leetcode/september/Nine.java
+++ src/main/java/org/example/leetcode/september/Nine.java
@@ -9,67 +9,39 @@ import java.util.Arrays;
 public class Nine {
 
     private static Integer MOD = 1_000_000_000 + 7;
-    private int delay;
-    private int forget;
-    private int[] memo;
 
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        // 最直接的想法，设计一个结构体出来  知道的时间 t,两个方法，一个是判断是否到延迟时间，生成一个新的结构体
-        // 还有一个方法是否判断是否遗忘，到了的话从知道的人列表中把这个结构体剔除
-        // 这里结构体最关键的元素就是知道这个秘密的天数
-
-        // 第一天，只有一个人知道秘密 这个人知道秘密的天数是1
-        // 延迟了 delay 天后才有其他人知道
-        this.delay = delay;
-        this.forget = forget;
-        int[] memo = new int[n + 1];
-        Arrays.fill(memo, -1);
-        this.memo = memo;
-        // n-delay天前知道秘密的人，n-forge之后还没有忘记，还会继续分享秘密
-        int noForgetDay = n - forget + 1;
-
-        int shareDay = n - delay;
-
-        int ans = 0;
-        for (int d = noForgetDay; d < n; d++) {
-            if (d <= shareDay) {
-                ans += (dfs(d) * 2) % MOD;
+        // 使用动态规划，也就是迭代的方式实现
+        // 这里的状态方程代表的是每天新增的知道秘密的人数
+        int[] dp = new int[n + 1];
+        // 第一天的时候只有一个人知道
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            if (i <= delay) {
+                // 这个时候还不能分享秘密
+                continue;
             } else {
-                ans += dfs(d);
+                int shareCount = 0;
+                for (int j = i - forget + 1; j <= i - delay; j++) {
+                    if (j <= 0) {
+                        continue;
+                    }
+                    shareCount += dp[j];
+                    shareCount %= MOD;
+                }
+                dp[i] = shareCount;
             }
+        }
+
+
+        // 第n天知道的人总数 noForgetDay =  n-forget+1；从noForgetDay开始这些天新增知道秘密的人数的和
+        int noForgetDay = n - forget + 1;
+        int ans = 0;
+        for (int k = noForgetDay; k <= n; k++) {
+            ans += dp[k];
             ans %= MOD;
         }
-
         return ans;
     }
 
-    // 我可以想到这个递归函数关键是这个递归函数的返回值怎么设计
-    // 这里返回值是 在 第n天的时候后新增了多少人知道秘密
-    public int dfs(int n) {
-        if (n < 1) {
-            return 0;
-        }
-        if (memo[n] != -1) {
-            return memo[n];
-        }
-        if (n == 1) {
-            memo[n] = 1;
-            return 1;
-        }
-        int noForgetDay = n - forget + 1;
-
-        int shareDay = n - delay;
-
-        int ans = 0;
-        for (int d = noForgetDay; d < n; d++) {
-            if (d <= shareDay) {
-                ans += dfs(d);
-                ans %= MOD;
-            }
-        }
-        ans %= MOD;
-        memo[n] = ans;
-        return ans;
-
-    }
 }
